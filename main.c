c12331e52996fe12e670fe99b3ffcfc2dc3bd344
est_calexx <carlxxsinenomine@gmail.com>
2026-04-09 22:01:55 +0800

    fuck c

--- main.c
+++ main.c
@@ -9,18 +9,33 @@ void insertLast(int arr[], int *n, int val) {
 
 int main() {
   unsigned long int MAX_RANGE = 4294967295;
+  srand((unsigned long int) time(NULL));
   // User input
-  unsigned long int N = rand() % (MAX_RANGE);
-  int x;
+  // int N = rand();
+  int N;
+  scanf("%d", &N);
+  int M;
+  printf("choose data generation meth: 1 or 2");
+  int arr[N];
 
+  scanf("%d", &M);
+  int F;
+  if(M==1) {
+  // First method
+    for(int i=0; i<N;i++) {
+      insertLast(arr, &i, rand());
+    }
+  } else {
+  // Sec Method 
+  int x;
   printf("Enter a value for x");
   scanf("%d", &x);
   if(x < 0) return -1;
-  int arr[N]; // If valid
   // For loop?
   for(int i = 0; i < N; i++) {
     // Prob
     insertLast(arr, &i, (x + i)); // array, index, value 
+  }
   }
   int arrLen = sizeof(arr) / sizeof(arr[0]);
   for (int i=0;i<arrLen; i++) {
